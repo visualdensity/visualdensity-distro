5023284b985239c8502ce076f98713e4a613e164
Wee Keat Chin <weekeat@visualdensity.com>
2015-03-22 13:48:22 +1100

    Initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+sendy-platform
+==============
+
+A Symfony project created on March 22, 2015, 11:30 am.
---
+++ src/Distro/CoreBundle/DistroCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Distro\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DistroCoreBundle extends Bundle
+{
+}
