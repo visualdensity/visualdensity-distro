df760865e6e8b39b0150fd23933f55ab222ce083
Wee Keat Chin <weekeat@visualdensity.com>
2015-03-22 17:56:55 +1100

    Subscribers bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Distro\CoreBundle\DistroCoreBundle(),
             new Distro\LinksManagerBundle\DistroLinksManagerBundle(),
+            new Distro\SubscribersManagerBundle\DistroSubscribersManagerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Distro/SubscribersManagerBundle/DistroSubscribersManagerBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Distro\SubscribersManagerBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DistroSubscribersManagerBundle extends Bundle
+{
+}
